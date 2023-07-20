94a7bcb3b6827976b8ace7e99c3daffc5f812dc2
teddy1016 <yagang1016@outlook.com>
2023-07-20 17:11:18 +0800

    simple chatgpt

---
+++ chatgpt/views.py
@@ -0,0 +1,29 @@
+
+from django.shortcuts import render
+from django.http import JsonResponse
+import openai
+  
+openai.api_key = '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'
+  
+def get_completion(prompt):
+    print(prompt)
+    query = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+  
+    response = query.choices[0].text
+    print(response)
+    return response
+  
+  
+def query_view(request):
+    if request.method == 'POST':
+        prompt = request.POST.get('prompt')
+        response = get_completion(prompt)
+        return JsonResponse({'response': response})
+    return render(request, 'index.html')
